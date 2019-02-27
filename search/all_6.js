2c2b5004770a7a03d9b0e9176b526d3903da2a19
Jose Hugo García <jgarcia@1605M010.icn2.net>
2019-02-27 10:38:31 +0100

    Hide namespace

--- search/all_6.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['kpm_5ferror_2ehpp',['kpm_error.hpp',['../kpm__error_8hpp.html',1,'']]],
-  ['kpm_5fgeo_2ehpp',['kpm_geo.hpp',['../kpm__geo_8hpp.html',1,'']]],
-  ['kpm_5fmom_2ehpp',['kpm_mom.hpp',['../kpm__mom_8hpp.html',1,'']]]
-];
--- search/all_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['orbitalspercell',['OrbitalsPercell',['../structqt_1_1kpm_1_1geo_1_1params.html#a3b00ffc0e0126748c2acb94d7774711a',1,'qt::kpm::geo::params']]]
-];
